fdd8c8e0636f591988fa0bc3c9b918225d2aaf0e
Daniel Werner <vernerd@gmail.com>
2018-05-24 14:55:08 +0200

    test cases

---
+++ tests/Unit/RoleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RoleTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testRoles()
+    {
+        $user = factory(User::class)->create([
+            'role_id' => User::ROLE_ADMIN
+        ]);
+
+        $this->actingAs($user);
+
+        $roles = User::getRoles();
+
+        $this->assertTrue( count($roles) === 2 );
+        $this->assertArrayNotHasKey(User::ROLE_SUPERADMIN, $roles);
+
+    }
+}
