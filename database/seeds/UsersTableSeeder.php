2f14350411a8565d58d35a4af828d490e9265ade
Daniel Werner <vernerd@gmail.com>
2018-06-20 15:04:03 +0200

    users table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => env('SEED_USER_NAME', str_random(10)),
+            'role_id' => \App\User::ROLE_SUPERADMIN,
+            'email' => env('SEED_USER_EMAIL', 'demo@email.com'),
+            'password' => bcrypt(env('SEED_USER_PASSWORD', 'demo'))
+        ]);
+    }
+}
