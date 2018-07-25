f9407ee9d5e1a8786157b3a829d2e795f82f8ae5
Daniel Werner <vernerd@gmail.com>
2018-07-25 11:04:42 +0200

    login design tweaks

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,19 +8,26 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
+                    <div class="form-group row">
+                        <div class="col-md-6 offset-md-3">
+                            <a href="{{ url('/login/google') }}" class="btn btn-danger col-12"><i class="fab fa-google"></i> Sign in with Google</a>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 offset-md-3">
+                            <a href="{{ url('/login/github') }}" class="btn btn-dark col-12"><i class="fab fa-github"></i> Sign in with GitHub</a>
+                        </div>
+                    </div>
+                    <div class="form-group offset-md-6">
+                        or
+                    </div>
+                    <hr>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
+                        <div class="form-group col-md-6 offset-md-3">
+                            <label for="email" class="col-form-label">{{ __('E-Mail Address') }}</label>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <a href="{{ url('/login/google') }}" class="btn btn-danger"><i class="fab fa-google"></i> Sign in with Google</a>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
+                            <div class="">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -31,10 +38,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="form-group col-md-6 offset-md-3">
+                            <label for="password" class="col-form-label">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -45,8 +52,8 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="form-group">
+                            <div class="col-md-6 offset-md-3">
                                 <div class="checkbox">
                                     <label>
                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
@@ -55,8 +62,8 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                        <div class="form-group mb-0">
+                            <div class="col-md-8 offset-md-3">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
