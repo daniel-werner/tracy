9975a7f7552b2c9bf4cb04258f3783efafb4178f
Daniel Werner <vernerd@gmail.com>
2018-09-19 15:16:35 +0200

    cors fixes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,9 +38,9 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
-            'bindings',
             \Barryvdh\Cors\HandleCors::class,
+            'throttle:60,1',
+            'bindings'
         ],
     ];
 
