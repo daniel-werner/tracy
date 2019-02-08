0d4c9bdc9bba27d678660aa9e8ebe8ce32ed7ed0
Daniel Werner <vernerd@gmail.com>
2019-02-08 20:06:55 +0100

    Created test for workout calculations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
+            'id' => 1,
             'name' => env('SEED_USER_NAME', str_random(10)),
             'role_id' => \App\User::ROLE_SUPERADMIN,
             'email' => env('SEED_USER_EMAIL', 'demo@email.com'),
---
+++ database/seeds/WorkoutsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class WorkoutsTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('workouts')->delete();
+        
+        \DB::table('workouts')->insert(array (
+            0 => 
+            array (
+                'id' => 13,
+                'external_id' => 1034753061,
+                'type' => 2,
+                'user_id' => 1,
+                'title' => 'Endomondo workout',
+                'time' => '2016-07-30 14:31:25',
+                'import_filename' => '',
+                'status' => 1,
+                'created_at' => '2018-09-19 19:06:21',
+                'updated_at' => '2018-09-19 19:06:21',
+            ),
+        ));
+
+    }
+}
