44396f100f6ddac16d943862f1dab3fac873f4e4
Daniel Werner <vernerd@gmail.com>
2019-12-26 11:58:36 +0100

    Upgrade Laravel 5.7 -> 5.8

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -15,7 +15,7 @@ class ApiTest extends TestCase
     /** @var User $user */
     protected $user;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
