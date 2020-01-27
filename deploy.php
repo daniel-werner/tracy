112e9c210d6392fad0c6b02f1cd98a04533b9691
Daniel Werner <vernerd@gmail.com>
2020-01-27 21:53:19 +0100

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -10,7 +10,7 @@ set('application', 'Tracy');
 set('repository', 'https://github.com/daniel-werner/tracy.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
+set('git_tty', false);
 
 // Shared files/dirs between deploys
 add('shared_files', []);
